50e772e71ded2538017b19a785a9348e7fcc1fff
YaeEun2 <129499535+YaeEun2@users.noreply.github.com>
2024-06-14 17:35:49 +0900

    addelevatorAnimation

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -5,23 +5,27 @@ using UnityEngine.AI;
 
 public class PathFinder : MonoBehaviour
 {
-    public GameObject target;
+    public GameObject target; //이동할 목표 지점
     NavMeshAgent agent;
-    LineRenderer lr;
+    LineRenderer lr; //경로
 
-
-    // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
 
         lr = this.GetComponent<LineRenderer>();
-        lr.startWidth = lr.endWidth = 1.0f;
+        lr.startWidth = lr.endWidth = 10;
         lr.material.color= Color.red;
-        lr.enabled = false ;
-       
+        lr.enabled = false ; //초기 비활성화
+
+        StartCoroutine(StartPathAfterDelay(4.0f));
     }
 
+    IEnumerator StartPathAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay); // delay 만큼 대기
+        makePath();
+    }
     public void makePath()
     {
         lr.enabled= true ;
@@ -36,23 +40,22 @@ public class PathFinder : MonoBehaviour
         for(int i = 1; i < length; i++)
             lr.SetPosition(i, agent.path.corners[i]);
     }
+
     IEnumerator makePathCoroutine()
     {
-        Debug.Log("Coroutine 시작됨!");
         agent.SetDestination(target.transform.position);
-        lr.SetPosition(0, this.transform.position);
+         lr.SetPosition(0, this.transform.position);
 
-        while (Vector3.Distance(this.transform.position, target.transform.position) > 0.1f)
-        {
-            lr.SetPosition(0, this.transform.position);
+          while (Vector3.Distance(this.transform.position, target.transform.position) > 0.1f)
+          {
+              lr.SetPosition(0, this.transform.position);
 
-            drawPath();
+              drawPath();
 
-            yield return null;
-        }
+              yield return null;
+          }
 
-        lr.enabled = false;
+          lr.enabled = false;
+      }
     }
-}
-
 
