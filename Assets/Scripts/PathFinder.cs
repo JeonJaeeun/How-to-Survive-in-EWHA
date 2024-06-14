eb3ccc93990f5bc469f81c7fe878a8685ecf389f
YaeEun2 <129499535+YaeEun2@users.noreply.github.com>
2024-06-14 13:49:55 +0900

    plus

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -7,10 +7,52 @@ public class PathFinder : MonoBehaviour
 {
     public GameObject target;
     NavMeshAgent agent;
+    LineRenderer lr;
+
+
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+
+        lr = this.GetComponent<LineRenderer>();
+        lr.startWidth = lr.endWidth = 1.0f;
+        lr.material.color= Color.red;
+        lr.enabled = false ;
+       
+    }
+
+    public void makePath()
+    {
+        lr.enabled= true ;
+        StartCoroutine(makePathCoroutine());
+    }
+
+    void drawPath()
+    {
+        int length = agent.path.corners.Length;
+
+        lr.positionCount = length;
+        for(int i = 1; i < length; i++)
+            lr.SetPosition(i, agent.path.corners[i]);
+    }
+    IEnumerator makePathCoroutine()
+    {
+        Debug.Log("Coroutine ½ÃÀÛµÊ!");
         agent.SetDestination(target.transform.position);
+        lr.SetPosition(0, this.transform.position);
+
+        while (Vector3.Distance(this.transform.position, target.transform.position) > 0.1f)
+        {
+            lr.SetPosition(0, this.transform.position);
+
+            drawPath();
+
+            yield return null;
+        }
+
+        lr.enabled = false;
     }
 }
+
+
---
+++ Assets/Scripts/test1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    public PathFinder pathFinder;
+
+    void Start()
+    {
+        pathFinder.makePath();
+    }
+}
