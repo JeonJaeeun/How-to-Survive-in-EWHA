0c92b2480501344d3da184b892ce8329e45cec6b
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2024-06-15 23:25:24 +0900

    startScenes 애니메이션 추가, 공대,ECC AR 힌트

---
+++ Assets/Scripts/ClickFlower_ECC2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickFlower_ECC2 : MonoBehaviour
+{
+
+ public void SceneChange()
+        {
+            SceneManager.LoadScene("1_Main"); // "Main" 씬으로 전환
+
+        }
+
+}
