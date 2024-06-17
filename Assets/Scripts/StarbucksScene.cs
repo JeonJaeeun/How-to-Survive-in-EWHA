f6c74fa53a44a3c07eb5bc3617faf9922039125a
OSSU <o.ss.u2056@gmail.com>
2024-06-18 07:05:02 +0900

    feat: 스탬프 획득 여부 저장

--- Assets/Scripts/StarbucksScene.cs
+++ Assets/Scripts/StarbucksScene.cs
@@ -20,6 +20,15 @@ public class StarbucksScene : MonoBehaviour
 
     void Start()
     {
+        if(PlayerPrefs.GetInt("Starbucks") == 1)
+        {
+            stamp.color = new Color(255f, 255f, 255f, 1f);
+            stampAnimator.SetBool("isEnd", true);
+        }
+        else
+        {
+            stamp.color = new Color(0f, 0f, 0f, 1f);
+        }
         if (ChatManager.manager.starbucks2)
         {
             mapButton.SetActive(true);
@@ -74,6 +83,7 @@ public class StarbucksScene : MonoBehaviour
     private void ChangeStampColor()
     {
         stamp.color = new Color(255f, 255f, 255f, 1f); // 변경할 색상을 설정합니다.
+        PlayerPrefs.SetInt("Starbucks", 1); // 스탬프 획득 여부를 저장합니다.
 
         // 1초 뒤에 chatModal 활성화
         Invoke(nameof(ShowChat), 1f);
