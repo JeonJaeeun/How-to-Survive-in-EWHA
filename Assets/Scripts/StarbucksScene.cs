05c49fc05354f6598ba87f69ca7b2fc38a5641d5
OSSU <o.ss.u2056@gmail.com>
2024-06-18 03:03:54 +0900

    feat: 스타벅스 스탬프 획득 후 잉여계단 장면 전환 추가

--- Assets/Scripts/StarbucksScene.cs
+++ Assets/Scripts/StarbucksScene.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StarbucksScene : MonoBehaviour
 {
     [SerializeField] private Animator coneAnimator;
     [SerializeField] private Animator mapAnimator;
+    [SerializeField] private Animator stampAnimator;
     [SerializeField] private GameObject mapButton;
     [SerializeField] private GameObject mapModal;
-    
+    [SerializeField] private Image stamp;
+
     void Start()
     {
-        if(InsideSceneManager.manager.CheckIsNavigationEnd())
+        if (InsideSceneManager.manager.CheckIsNavigationEnd())
         {
             mapButton.SetActive(true);
             mapModal.SetActive(true);
             mapAnimator.SetBool("stop", true);
             coneAnimator.SetBool("stop", true);
         }
-        else {
+        else
+        {
             coneAnimator.SetBool("stop", false);
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (stampAnimator.GetBool("isEnd"))
+        {
+            ChangeStampColor();
+        }
+    }
+
+    // stampAnimator의 애니메이션 이벤트에서 호출할 함수
+    public void ChangeStampColor()
+    {
+        stamp.color = new Color(255f, 255f, 255f, 1f); // 변경할 색상을 설정합니다.
+    }
+
+    public void SceneChange()
+    {
+        Debug.Log("버튼 클릭");
+        SceneManager.LoadScene(InsideSceneManager.manager.ECC_STAIRS);
+    }
+
+    public void ClickMapButton()
+    {
+        // 맵 모달이 활성화 되어 있을 경우 비활성화
+        if (mapModal.activeSelf)
+        {
+            mapModal.SetActive(false);
+            return;
+        }
+        mapModal.SetActive(true);
     }
 }
